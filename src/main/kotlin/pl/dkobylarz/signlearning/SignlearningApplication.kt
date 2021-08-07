5513431cdb94314661dfe73afd3fa911f2bf9062
dawid98103 <dawid.kobylarz@protonmail.com>
2021-08-07 11:47:30 +0200

    initial commit

---
+++ src/main/kotlin/pl/dkobylarz/signlearning/SignlearningApplication.kt
@@ -0,0 +1,11 @@
+package pl.dkobylarz.signlearning
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SignlearningApplication
+
+fun main(args: Array<String>) {
+	runApplication<SignlearningApplication>(*args)
+}
---
+++ src/test/kotlin/pl/dkobylarz/signlearning/SignlearningApplicationTests.kt
@@ -0,0 +1,13 @@
+package pl.dkobylarz.signlearning
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SignlearningApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
